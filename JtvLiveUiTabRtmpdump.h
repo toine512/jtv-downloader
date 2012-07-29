9c4dfc7989c5b8d7ddd3c427e5e25fe8df94a93f
toine512 <toine512@gmail.com>
2012-07-29 16:46:38 +0200

    License notice

--- JtvLiveUiTabRtmpdump.h
+++ JtvLiveUiTabRtmpdump.h
@@ -1,3 +1,21 @@
+/* This file is part of "Jtv live downloader"
+ *
+ * Copyright (C) 2012 toine512 <toine512@gmail.com>
+ *
+ * "Jtv live downloader" is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * "Jtv live downloader" is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with "Jtv live downloader".  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 #ifndef JTVLIVEUITABRTMPDUMP_H
 #define JTVLIVEUITABRTMPDUMP_H
 
